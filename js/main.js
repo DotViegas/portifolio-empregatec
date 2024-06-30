43b89d937e6bb80f53921a6b24bf112867d427ec
DotViegas <joaovictorviegas53@gmail.com>
2024-06-30 10:46:50 -0400

    inserindo scroll suave na pagina

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    orientation: 'vertical',
+    gestureOrientation: 'vertical',
+    smoothWheel: true,
+    wheelMultiplier: 1,
+    smoothTouch: false,
+    touchMultiplier: 2,
+    infinite: false,
+  })
+
+  function raf(time) {
+    lenis.raf(time)
+     requestAnimationFrame(raf)
+  }
+
+  requestAnimationFrame(raf)
